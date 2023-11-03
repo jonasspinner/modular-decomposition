9ced472c5b50162ec469dedf26726c450cd2caff
Jonas Spinner <jonas.spinner@student.kit.edu>
2023-11-03 16:08:51 +0100

    Add merging of ancestors with same node kind

--- algorithms/ms00/src/lib.rs
+++ algorithms/ms00/src/lib.rs
@@ -1,7 +1,9 @@
+#![allow(dead_code)]
+
 use petgraph::graph::{DiGraph, NodeIndex, UnGraph};
 use petgraph::visit::EdgeRef;
 use common::modular_decomposition::MDNodeKind;
-use crate::seq::algos::{Kind, TreeNode};
+use crate::seq::algos::{Kind, TreeNode, TreeNodeIndex};
 
 mod linked_list;
 mod seq;
@@ -21,14 +23,13 @@ pub struct Prepared {
 
 pub fn prepare<N, E>(graph: &UnGraph<N, E>) -> Prepared
 {
-    let edges : Vec<_> = graph.edge_references().map(|e| {
+    let edges: Vec<_> = graph.edge_references().map(|e| {
         let u = seq::graph::NodeIndex::new(e.source().index());
         let v = seq::graph::NodeIndex::new(e.target().index());
         (u, v)
     }).collect();
-    let mut graph = seq::graph::Graph::from_edges(
-        graph.node_count(), edges.iter().copied());
-    let mut partition = seq::partition::Partition::new(graph.node_count());
+    let graph = seq::graph::Graph::from_edges(graph.node_count(), edges.iter().copied());
+    let partition = seq::partition::Partition::new(graph.node_count());
     Prepared { graph, partition }
 }
 
@@ -42,29 +43,45 @@ impl Prepared {
         let p = seq::partition::Part::new(&self.partition);
         let mut tree = Vec::with_capacity(2 * self.graph.node_count());
 
-        let root = seq::algos::TreeNodeIndex::new(0);
-        tree.push(seq::algos::TreeNode::default());
-        seq::algos::modular_decomposition(&mut self.graph, p, &mut self.partition, &mut tree, root);
+        if self.graph.node_count() != 0 {
+            let root = seq::algos::TreeNodeIndex::new(0);
+            tree.push(seq::algos::TreeNode::default());
+            seq::algos::modular_decomposition(&mut self.graph, p, &mut self.partition, &mut tree, root);
+        }
         Computed { tree }
     }
 }
 
 impl Computed {
     pub fn finalize(&self) -> DiGraph<MDNodeKind, ()> {
-        let mut md = DiGraph::new();
-        for i in 0..self.tree.len() {
-            let kind = match self.tree[i].kind {
+        let add_node = |md: &mut DiGraph<MDNodeKind, ()>, i: TreeNodeIndex| -> NodeIndex {
+            let kind = match self.tree[i.index()].kind {
                 Kind::Prime => { MDNodeKind::Prime }
                 Kind::Series => { MDNodeKind::Series }
                 Kind::Parallel => { MDNodeKind::Parallel }
-                Kind::Vertex(u) => { MDNodeKind::Vertex(u.index())}
+                Kind::Vertex(u) => { MDNodeKind::Vertex(u.index()) }
                 Kind::UnderConstruction => { panic!() }
             };
-            md.add_node(kind);
-        }
-        for i in 0..self.tree.len() {
-            for j in &self.tree[i].children {
-                md.add_edge(NodeIndex::new(i), NodeIndex::new(j.index()), ());
+            md.add_node(kind)
+        };
+
+        let mut md = DiGraph::new();
+
+        if self.tree.is_empty() { return md; }
+
+        let root = TreeNodeIndex::new(0);
+        let mut stack = vec![root];
+        let mut map = vec![NodeIndex::end(); self.tree.len()];
+        map[root.index()] = add_node(&mut md, root);
+        while let Some(i) = stack.pop() {
+            for &j in &self.tree[i.index()].children {
+                stack.push(j);
+                if self.tree[j.index()].kind == self.tree[i.index()].kind && self.tree[j.index()].kind != Kind::Prime {
+                    map[j.index()] = map[i.index()];
+                } else {
+                    map[j.index()] = add_node(&mut md, j);
+                    md.add_edge(map[i.index()], map[j.index()], ());
+                }
             }
         }
         md
@@ -73,4 +90,63 @@ impl Computed {
 
 pub fn modular_decomposition<N, E>(graph: &UnGraph<N, E>) -> DiGraph<MDNodeKind, ()> {
     prepare(graph).compute().finalize()
+}
+
+
+mod test {
+    use std::path::Path;
+    use petgraph::dot::{Config, Dot};
+    use petgraph::graph::{DiGraph, UnGraph};
+    use petgraph::visit::IntoNodeReferences;
+    use common::instances::ted08_test0;
+    use common::modular_decomposition::MDNodeKind;
+    use crate::modular_decomposition;
+
+    #[test]
+    fn modular_decomposition_test() {
+        let graph = UnGraph::<(), ()>::from_edges([(0, 1), (1, 2), (2, 3)]);
+        let md = modular_decomposition(&graph);
+
+        println!("{:?}", Dot::with_config(&md, &[Config::EdgeNoLabel]));
+    }
+
+    #[test]
+    fn test0() {
+        let graph = ted08_test0();
+
+        let md = modular_decomposition(&graph);
+
+        let count_node_kinds = |tree: &DiGraph<MDNodeKind, ()>| -> (usize, usize, usize, usize) {
+            tree.node_references()
+                .fold((0, 0, 0, 0),
+                      |(prime, series, parallel, vertex), (_, k)| {
+                          match k {
+                              MDNodeKind::Prime => (prime + 1, series, parallel, vertex),
+                              MDNodeKind::Series => (prime, series + 1, parallel, vertex),
+                              MDNodeKind::Parallel => (prime, series, parallel + 1, vertex),
+                              MDNodeKind::Vertex(_) => (prime, series, parallel, vertex + 1)
+                          }
+                      })
+        };
+
+        assert_eq!(md.node_count(), 27);
+        assert_eq!(count_node_kinds(&md), (2, 4, 3, 18));
+    }
+
+    #[test]
+    fn empty() {
+        let graph = UnGraph::<(), ()>::new_undirected();
+        let md = modular_decomposition(&graph);
+        assert_eq!(md.node_count(), 0);
+        assert_eq!(md.edge_count(), 0);
+    }
+
+    #[test]
+    fn exact_053() {
+        let path = Path::new("../../hippodrome/instances/pace2023/exact_053.gr");
+        let graph = common::io::read_pace2023(path).unwrap();
+        let md = modular_decomposition(&graph);
+
+        println!("{:?}", Dot::with_config(&md, &[Config::EdgeNoLabel]));
+    }
 }
