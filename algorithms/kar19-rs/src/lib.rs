a6cd0599beb22223a7d2a32baee4f77157467e21
Jonas Spinner <jonas.spinner@student.kit.edu>
2024-01-03 19:08:21 +0100

    Add faster factorizing permutation, count and removal of dummy nodes

--- algorithms/kar19-rs/src/lib.rs
+++ algorithms/kar19-rs/src/lib.rs
@@ -5,6 +5,7 @@ use std::fmt::{Debug, Formatter};
 use petgraph::graph::{DiGraph, NodeIndex, UnGraph};
 use common::modular_decomposition::MDNodeKind;
 use tracing::{span, Level};
+use crate::factorizing_permutation::seq::Permutation;
 
 
 #[macro_export]
@@ -19,13 +20,7 @@ fn strong_module_tree(graph: &UnGraph<(), ()>) -> StrongModuleTree {
     let p = {
         let _span_ = span!(Level::INFO, "factorizing_permutation").entered();
 
-        let p = factorizing_permutation::kar19::factorizing_permutation(graph);
-
-        //let mut state = playground::factorizing_permutation::v2::State::new(graph);
-        //state.partition_refinement();
-        //let p: Vec<_> = state.permutation().collect();
-
-        p
+        factorizing_permutation::seq::factorizing_permutation(graph)
     };
 
     let neighbors: Vec<HashSet<NodeIndex>> = graph.node_indices()
@@ -41,36 +36,13 @@ fn strong_module_tree(graph: &UnGraph<(), ()>) -> StrongModuleTree {
     let mut lc: Vec<_> = (0..n - 1).collect();
     let mut uc: Vec<_> = (1..n).collect();
 
-    trace!("{:?}", d1(&p));
+    trace!("{:?}", dp(&p));
     trace!("op: {:?}", op);
     trace!("cl: {:?}", cl);
 
     {
         let _span_ = span!(Level::INFO, "count").entered();
-
-        for j in 0..n - 1 {
-            for i in 0..j {
-                let adj_p_i = &neighbors[p[i].index()];
-                if adj_p_i.contains(&p[j]) == adj_p_i.contains(&p[j + 1]) {
-                    continue;
-                }
-                op[i] += 1;
-                cl[j] += 1;
-                lc[j] = i;
-                break;
-            }
-            let j = j + 1;
-            for i in (j + 1..n).rev() {
-                let adj_p_i = &neighbors[p[i].index()];
-                if adj_p_i.contains(&p[j - 1]) == adj_p_i.contains(&p[j]) {
-                    continue;
-                }
-                op[j] += 1;
-                cl[i] += 1;
-                uc[j - 1] = i;
-                break;
-            }
-        }
+        count_v2(&mut op, &mut cl, &mut lc, &mut uc, &neighbors, &p);
     }
 
     trace!("op: {:?}", op);
@@ -78,20 +50,7 @@ fn strong_module_tree(graph: &UnGraph<(), ()>) -> StrongModuleTree {
 
     {
         let _span_ = span!(Level::INFO, "remove_non_module_dummy_nodes").entered();
-        let mut s = Vec::with_capacity(n);
-        for j in 0..n {
-            for _ in 0..op[j] { s.push(j); }
-            for _ in 0..cl[j] {
-                let i = s.pop().unwrap();
-                if i < j {
-                    let l = (i..j).map(|k| lc[k]).min().unwrap();
-                    let u = (i..j).map(|k| uc[k]).max().unwrap();
-                    if i <= l && u <= j { continue; }
-                }
-                op[i] -= 1;
-                cl[j] -= 1;
-            }
-        }
+        remove_non_module_dummy_nodes_v2(&mut op, &mut cl, &mut lc, &mut uc);
     }
 
     trace!("op: {:?}", op);
@@ -141,7 +100,6 @@ fn strong_module_tree(graph: &UnGraph<(), ()>) -> StrongModuleTree {
 
         for j in 0..n {
             s.extend(std::iter::repeat(j).take(op[j]));
-            //for _ in 0..op[j] { s.push(j); }
             let mut i_ = usize::MAX;
             for _ in 0..cl[j] {
                 let i = s.pop().unwrap();
@@ -159,7 +117,7 @@ fn strong_module_tree(graph: &UnGraph<(), ()>) -> StrongModuleTree {
 
     trace!("op: {:?}", op);
     trace!("cl: {:?}", cl);
-    trace!("p: {:?}", d1(&p));
+    trace!("p: {:?}", dp(&p));
 
     let mut s = vec![vec![]];
     {
@@ -167,7 +125,7 @@ fn strong_module_tree(graph: &UnGraph<(), ()>) -> StrongModuleTree {
 
         for (j, x) in p.iter().enumerate() {
             for _ in 0..op[j] { s.push(vec![]); }
-            s.last_mut().unwrap().push(NodeInProgress::Node(*x));
+            s.last_mut().unwrap().push(NodeInProgress::Node(x));
             for _ in 0..cl[j] {
                 let n = s.pop().unwrap();
                 s.last_mut().unwrap().push(NodeInProgress::Vec(n[0].first_leaf(), n));
@@ -194,27 +152,130 @@ fn strong_module_tree(graph: &UnGraph<(), ()>) -> StrongModuleTree {
     t
 }
 
+
+#[allow(dead_code)]
+fn count_v1(op: &mut [usize], cl: &mut [usize], lc: &mut [usize], uc: &mut [usize], neighbors: &[HashSet<NodeIndex>], p: &Permutation) {
+    let n = p.len();
+    for j in 0..n - 1 {
+        for i in 0..j {
+            let adj_p_i = &neighbors[p[i].index()];
+            if adj_p_i.contains(&p[j]) == adj_p_i.contains(&p[j + 1]) {
+                continue;
+            }
+            op[i] += 1;
+            cl[j] += 1;
+            lc[j] = i;
+            break;
+        }
+        let j = j + 1;
+        for i in (j + 1..n).rev() {
+            let adj_p_i = &neighbors[p[i].index()];
+            if adj_p_i.contains(&p[j - 1]) == adj_p_i.contains(&p[j]) {
+                continue;
+            }
+            op[j] += 1;
+            cl[i] += 1;
+            uc[j - 1] = i;
+            break;
+        }
+    }
+}
+
+fn count_v2(op: &mut [usize], cl: &mut [usize], lc: &mut [usize], uc: &mut [usize], neighbors: &[HashSet<NodeIndex>], p: &Permutation) {
+    let n = p.len();
+    for j in 0..n - 1 {
+        let (mut min_i, mut max_i) = (None, None);
+        for &x in neighbors[p[j].index()].symmetric_difference(&neighbors[p[j + 1].index()]) {
+            let i = p.position(x);
+            if i < j && (min_i.map_or(true, |k| k > i)) {
+                min_i = Some(i);
+            }
+            if i > j + 1 && (max_i.map_or(true, |k| k < i)) {
+                max_i = Some(i);
+            }
+        }
+        if let Some(i) = min_i {
+            assert!(i < j);
+            debug_assert_ne!(neighbors[p[i].index()].contains(&p[j]), neighbors[p[i].index()].contains(&p[j + 1]));
+            op[i] += 1;
+            cl[j] += 1;
+            lc[j] = i;
+        }
+        if let Some(i) = max_i {
+            assert!(i > j + 1 && i < n);
+            debug_assert_ne!(neighbors[p[i].index()].contains(&p[j]), neighbors[p[i].index()].contains(&p[j + 1]));
+            op[j + 1] += 1;
+            cl[i] += 1;
+            uc[j] = i;
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn remove_non_module_dummy_nodes_v1(op: &mut [usize], cl: &mut [usize], lc: &mut [usize], uc: &mut [usize]) {
+    let n = op.len();
+    let mut s = Vec::with_capacity(n);
+    for j in 0..n {
+        for _ in 0..op[j] { s.push(j); }
+        for _ in 0..cl[j] {
+            let i = s.pop().unwrap();
+            if i < j {
+                let l = (i..j).map(|k| lc[k]).min().unwrap();
+                let u = (i..j).map(|k| uc[k]).max().unwrap();
+                if i <= l && u <= j { continue; }
+            }
+            op[i] -= 1;
+            cl[j] -= 1;
+        }
+    }
+}
+
+fn remove_non_module_dummy_nodes_v2(op: &mut [usize], cl: &mut [usize], lc: &mut [usize], uc: &mut [usize]) {
+    let n = op.len();
+    let mut s = Vec::with_capacity(n);
+    for j in 0..n {
+        s.push((j, op[j]));
+        let mut k = cl[j];
+        while k > 0 {
+            let (i, count) = s.pop().unwrap();
+            if i < j {
+                let l = (i..j).map(|k| lc[k]).min().unwrap();
+                let u = (i..j).map(|k| uc[k]).max().unwrap();
+                if i <= l && u <= j {
+                    if k >= count { k -= count; } else {
+                        s.push((i, count - k));
+                        break;
+                    }
+                    continue;
+                }
+            }
+            op[i] -= count.min(k);
+            cl[j] -= count.min(k);
+            if k >= count { k -= count; } else {
+                s.push((i, count - k));
+                break;
+            }
+        }
+    }
+}
+
 #[derive(PartialEq)]
 enum Kind {
-    Complete,
+    Series,
     Parallel,
-    #[allow(dead_code)]
-    Linear,
     Prime,
 }
 
 struct StrongModuleTree {
     kind: Kind,
-    edge: Option<(bool, bool)>,
     nodes: Vec<Node>,
 }
 
 impl Debug for StrongModuleTree {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let parens: [char; 2] = match self.kind {
-            Kind::Complete => { ['(', ')'] }
+            Kind::Series => { ['(', ')'] }
             Kind::Parallel => { ['|', '|'] }
-            Kind::Linear => { ['[', ']'] }
             Kind::Prime => { ['{', '}'] }
         };
         write!(f, "{}", parens[0])?;
@@ -252,11 +313,7 @@ impl Debug for NodeInProgress {
         match self {
             NodeInProgress::Node(x) => { write!(f, "{}", x.index()) }
             NodeInProgress::Vec(_first, v) => {
-                let mut l = f.debug_list();
-                for x in v {
-                    l.entry(x);
-                }
-                l.finish()
+                f.debug_list().entries(v).finish()
             }
         }
     }
@@ -271,7 +328,7 @@ impl NodeInProgress {
     }
 }
 
-fn classify_nodes(t: &Vec<NodeInProgress>, neighbors: &Vec<HashSet<NodeIndex>>) -> StrongModuleTree {
+fn classify_nodes(t: &Vec<NodeInProgress>, neighbors: &[HashSet<NodeIndex>]) -> StrongModuleTree {
     let n = t.len();
 
     let nodes: Vec<_> = t.iter().map(|n| n.first_leaf()).collect();
@@ -283,13 +340,13 @@ fn classify_nodes(t: &Vec<NodeInProgress>, neighbors: &Vec<HashSet<NodeIndex>>) 
         }
     }
 
-    let edge = Some((false, false));
-    let kind = if m == 0 { Kind::Parallel } else if 2 * m == n * (n - 1) { Kind::Complete } else { Kind::Prime };
+    assert!(2 * m <= n * (n - 1));
+    let kind = if m == 0 { Kind::Parallel } else if 2 * m == n * (n - 1) { Kind::Series } else { Kind::Prime };
     let nodes = t.iter().map(|x| match x {
         NodeInProgress::Node(x) => { Node::Node(*x) }
         NodeInProgress::Vec(_, x) => { Node::Tree(classify_nodes(x, neighbors)) }
     }).collect();
-    StrongModuleTree { kind, edge, nodes }
+    StrongModuleTree { kind, nodes }
 }
 
 fn delete_weak_modules(t: &mut StrongModuleTree) {
@@ -299,7 +356,7 @@ fn delete_weak_modules(t: &mut StrongModuleTree) {
         let x = &mut t.nodes[i];
         let Node::Tree(x) = x else { continue; };
         delete_weak_modules(x);
-        if !(t.kind == x.kind && x.kind != Kind::Prime && t.edge == x.edge) { continue; }
+        if !(t.kind == x.kind && x.kind != Kind::Prime) { continue; }
         let mut nodes = std::mem::take(&mut x.nodes);
         let x_len = nodes.len();
         t.nodes.append(&mut nodes);
@@ -318,6 +375,10 @@ pub(crate) fn d1(p: &[NodeIndex]) -> Vec<usize> {
     p.iter().map(|u| u.index()).collect()
 }
 
+pub(crate) fn dp(p: &factorizing_permutation::seq::Permutation) -> Vec<usize> {
+    p.iter().map(|u| u.index()).collect()
+}
+
 pub fn modular_decomposition(graph: &UnGraph<(), ()>) -> DiGraph<MDNodeKind, ()> {
     let tree = strong_module_tree(graph);
 
@@ -331,9 +392,8 @@ pub fn modular_decomposition(graph: &UnGraph<(), ()>) -> DiGraph<MDNodeKind, ()>
             }
             Node::Tree(tree) => {
                 let kind = match tree.kind {
-                    Kind::Complete => { MDNodeKind::Series }
+                    Kind::Series => { MDNodeKind::Series }
                     Kind::Parallel => { MDNodeKind::Parallel }
-                    Kind::Linear => { panic!() }
                     Kind::Prime => { MDNodeKind::Prime }
                 };
                 let u = md.add_node(kind);
