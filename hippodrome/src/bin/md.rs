c04867b95f591e4a3f71e4a52ddee97cb0a81dc5
Jonas Spinner <jonas.spinner@student.kit.edu>
2024-01-22 15:36:06 +0100

    Add stats output

--- hippodrome/src/bin/md.rs
+++ hippodrome/src/bin/md.rs
@@ -1,19 +1,17 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufWriter, Write};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::time::Instant;
 use clap::{Parser, ValueEnum};
-use petgraph::visit::IntoNodeReferences;
 use tracing::Level;
 use tracing_subscriber::fmt::format::FmtSpan;
 use tracing_subscriber::FmtSubscriber;
 use tracing_subscriber::util::SubscriberInitExt;
 use common::io::{GraphFileType, read_edgelist, read_metis, read_pace2023, write_md_tree_adj};
-use common::modular_decomposition::MDNodeKind;
 
 
-#[derive(Debug, Clone, Eq, PartialEq, ValueEnum)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, ValueEnum)]
 enum Algo {
     Miz23Rust,
     Miz23Cpp,
@@ -38,8 +36,20 @@ struct Cli {
 }
 
 
+fn write_stats(stats: &Option<PathBuf>, input: &Path, algo: Algo, time: Option<std::time::Duration>, status: &str) -> Result<(), Box<dyn Error>> {
+    let Some(stats) = stats else { return Ok(()); };
+    let mut out = BufWriter::new(File::create(stats)?);
+    let input = input.file_name().unwrap().to_string_lossy();
+    let algo = algo.to_possible_value().unwrap().get_name().to_string();
+    let time = time.map(|t| t.as_secs_f64()).unwrap_or(f64::NAN);
+    writeln!(out, "{{\"input\": \"{input}\", \"algo\": \"{algo}\", \"time\": {time}, \"status\": \"{status}\"}}")?;
+    Ok(())
+}
+
+
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
+    write_stats(&cli.stats, &cli.input, cli.algo, None, "unfinished")?;
     let graph = match cli.input_type {
         GraphFileType::Pace2023 => read_pace2023(&cli.input)?,
         GraphFileType::Metis => read_metis(&cli.input)?,
@@ -50,6 +60,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         let subscriber = FmtSubscriber::builder()
             .with_max_level(level)
             .with_span_events(FmtSpan::CLOSE)
+            //.json()
             .finish();
 
         subscriber.init();
@@ -81,30 +92,10 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     };
 
-    let (prime, series, parallel, vertex) = md.node_references()
-        .fold((0, 0, 0, 0),
-              |(prime, series, parallel, vertex), (_, k)| {
-                  match k {
-                      MDNodeKind::Prime => (prime + 1, series, parallel, vertex),
-                      MDNodeKind::Series => (prime, series + 1, parallel, vertex),
-                      MDNodeKind::Parallel => (prime, series, parallel + 1, vertex),
-                      MDNodeKind::Vertex(_) => (prime, series, parallel, vertex + 1)
-                  }
-              });
-
     if let Some(output) = cli.output {
         let mut out = BufWriter::new(File::create(output)?);
         write_md_tree_adj(&mut out, &md)?;
     }
-    if let Some(stats) = cli.stats {
-        let mut out = BufWriter::new(File::create(stats)?);
-        writeln!(out, "input {}", cli.input.file_name().unwrap().to_string_lossy())?;
-        writeln!(out, "algo {:#?}", cli.algo.to_possible_value().unwrap().get_name())?;
-        writeln!(out, "time {}", t.as_micros())?;
-        writeln!(out, "num_prime {}", prime)?;
-        writeln!(out, "num_series {}", series)?;
-        writeln!(out, "num_parallel {}", parallel)?;
-        writeln!(out, "num_vertex {}", vertex)?;
-    }
+    write_stats(&cli.stats, &cli.input, cli.algo, Some(t), "finished")?;
     Ok(())
 }
