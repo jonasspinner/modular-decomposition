158b257e852e9059e66d46f895bb7f72ac63d31c
Jonas Spinner <jonas.spinner@student.kit.edu>
2024-01-22 15:40:01 +0100

    Modify node output

--- common/src/modular_decomposition.rs
+++ common/src/modular_decomposition.rs
@@ -1,7 +1,20 @@
-#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
+use std::fmt::{Debug, Formatter};
+
+#[derive(Copy, Clone, Hash, Eq, PartialEq, Ord, PartialOrd)]
 pub enum MDNodeKind {
     Prime,
     Series,
     Parallel,
     Vertex(usize),
+}
+
+impl Debug for MDNodeKind {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            MDNodeKind::Prime => { write!(f, "Prime") }
+            MDNodeKind::Series => { write!(f, "Series") }
+            MDNodeKind::Parallel => { write!(f, "Parallel") }
+            MDNodeKind::Vertex(v) => { write!(f, "{v}")}
+        }
+    }
 }
